09a80bc01824952f9160fa82502dc2078821a502
Johnny Saniat <Jcsaniat@gmail.com>
2024-05-18 11:31:51 -0500

    Setup File Structure

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,27 +6,40 @@ import {
   Container,
   Nav,
   Button,
+  Image,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 
 export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar id="Navigation" collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand className="d-flex align-items-center">
+            <Image className="navbar-logo" src="https://i.ibb.co/Cwgk06t/Bleebos-Bistro-Nav.png" fluid />
+          </Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto align-items-center">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
+            <Link passHref href="/items">
+              <Nav.Link>Yums</Nav.Link>
             </Link>
-            <Link passHref href="/delete-me">
-              <Nav.Link>Delete Me</Nav.Link>
+            <Link passHref href="/revenue">
+              <Nav.Link>Revenue</Nav.Link>
             </Link>
-            <Button variant="danger" onClick={signOut}>
+            <Link passHref href="/order/new">
+              <Nav.Link>New Order</Nav.Link>
+            </Link>
+          </Nav>
+          <Nav className="align-items-center">
+            <Link passHref href="/orders">
+              <Nav.Link className="d-flex align-items-center">
+                <Image src="https://i.ibb.co/BfgBF1d/vecteezy-shopping-cart-creative-icon-design-31832783.png" alt="Bag Icon" fluid className="navbar-bag-icon" id="cart-icon" />
+              </Nav.Link>
+            </Link>
+            <Button variant="success" id="signout-nav" onClick={signOut}>
               Sign Out
             </Button>
           </Nav>
